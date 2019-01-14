e8248e2b505e646945b10f6ef034dcedcd92f361
abender <bender.adam@netpositive.hu>
2019-01-14 11:36:59 +0100

    ignore vscode history plugin backups

--- jest.config.js
+++ jest.config.js
@@ -139,9 +139,10 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    ".history/"
+  ],
 
   // The regexp pattern Jest uses to detect test files
   // testRegex: "",
